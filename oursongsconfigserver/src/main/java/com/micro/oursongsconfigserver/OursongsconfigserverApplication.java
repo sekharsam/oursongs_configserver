585fab9e733b000d8f72df83e02b086a8fda169e
sekharsam <samsomnath@gmail.com>
2021-07-13 01:13:06 +0530

    # WARNING: head commit changed in the meantime
    config server

---
+++ oursongsconfigserver/src/main/java/com/micro/oursongsconfigserver/OursongsconfigserverApplication.java
@@ -0,0 +1,13 @@
+package com.micro.oursongsconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OursongsconfigserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OursongsconfigserverApplication.class, args);
+	}
+
+}
---
+++ oursongsconfigserver/src/test/java/com/micro/oursongsconfigserver/OursongsconfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.micro.oursongsconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OursongsconfigserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
